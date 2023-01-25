5cf225ff9208e5ddb148ce2c826f71ea7c5fc458
ertyant24 <erikciertugrul@gmail.com>
2023-01-25 16:59:33 +0300

    Done ...

--- StudentsWebAPI/Controllers/StudentController.cs
+++ StudentsWebAPI/Controllers/StudentController.cs
@@ -29,14 +29,38 @@ namespace StudentsWebAPI.Controllers
 		{
 			Student student = _context.Students.FirstOrDefault(x => x.Id == id);
 
-			if(student == null)
+			if (student == null)
 			{
 				return NotFound();
 			}
-			
+
 
 			return Ok(student);
 		}
+
+		[HttpGet("{order}/{count}")]
+		public IActionResult GetListByCount(string order, int count)
+		{
+			List<Student> student = null;
+
+			if (order == "asc")
+			{
+				student = _context.Students.OrderBy(x => x.Age).Take(count).ToList();
+			}
+
+			if (order == "desc")
+			{
+				student = _context.Students.OrderByDescending(x => x.Age).Take(count).ToList();
+			}
+
+			if (order == "asc")
+			{
+				student = _context.Students.OrderBy(x => x.Id).ToList();
+			}
+
+			return Ok(student);
+		}
+
 
 		[HttpPost]
 		public IActionResult CreateStudent(CreateStudentModel model)
@@ -56,9 +80,48 @@ namespace StudentsWebAPI.Controllers
 
 			_context.Students.Add(student);
 			_context.SaveChanges();
-			
+
 
 			return Created("", model);
+		}
+
+		[HttpPut("update/{id}")]
+		public IActionResult UpdateStudent([FromRoute] int id, [FromBody] UpdateStudentModel model)
+		{
+			Student student = _context.Students.FirstOrDefault(x => x.Id == id);
+
+			if (student == null)
+			{
+				return NotFound();
+			}
+
+			student.Computer = model.Computer;
+			student.College = model.College;
+			student.Age = model.Age;
+			student.IsSuccess = model.IsSuccess;
+			student.FullName = model.FullName;
+
+			_context.SaveChanges();
+
+			return Ok(student);
+		}
+
+		[HttpDelete("delete/{id}")]
+		public IActionResult DeleteStudent([FromRoute] int id)
+		{
+			Student student = _context.Students.Find(id);
+
+			if(student != null)
+			{
+				_context.Students.Remove(student);
+				_context.SaveChanges();
+			}
+			else
+			{
+				return NotFound();
+			}
+
+			return Ok(student);
 		}
 	}
 }
